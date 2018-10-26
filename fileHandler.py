161f811451e31f7b40ad389f4678209292148ea1
Michael Cole <mcole042891@gmail.com>
2018-10-26 08:56:25 -0500

    Updated filehandler.py to not add spaces after commas when creating csv files

--- fileHandler.py
+++ fileHandler.py
@@ -6,10 +6,10 @@ def createFilesAndHeaders(userDict, startDate):
         print(f'Creating {user}\'s log file.')
         with open(filename, 'a+') as f:
             f.write('datetime')
-            f.write(', ')
+            f.write(',')
             for feature in userDict[user].keys():
                 f.write(feature)
-                f.write(', ')
+                f.write(',')
             f.write('\n')
 
 def logData(userDict, startDate):
@@ -17,10 +17,10 @@ def logData(userDict, startDate):
     for user in userDict:
         filename = f'./Logs/{user}Log{startDate}.csv'
         print(f'Logging {user}\'s data.')
-        with open(filename, 'a+') as f:
+        with open(filename, 'a') as f:
             f.write(logTime)
-            f.write(', ')
+            f.write(',')
             for info in userDict[user].values():
                 f.write(str(info))
-                f.write(', ')
+                f.write(',')
             f.write('\n')
